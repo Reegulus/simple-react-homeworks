fce093cc4ea88627c09704602ced453291ac1826
Egorych <mikhalevichegor@gmail.com>
2021-03-21 21:00:51 +0300

    Message.module.css layout

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -12,17 +12,17 @@ export function Message(props: MessageDataPropsType) {
     return (
 
         <div className={s.message}>
-            <div>
+            <div className={s.img}>
                 <img src='https://avatanplus.com/files/resources/original/57b3fe6c4059a1569719d6e8.png' alt=""/>
             </div>
             <div className={s.block_content}>
-                <div>
+                <div className={s.nameItem}>
                     {props.name}
                 </div>
-                <div>
+                <div className={s.messageItem}>
                     {props.message}
                 </div>
-                <div>
+                <div className={s.timeItem}>
                     {props.time}
                 </div>
             </div>

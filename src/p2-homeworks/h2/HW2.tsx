fa477377ce14898966f119469982148e329f0608
Egorych <mikhalevichegor@gmail.com>
2021-05-24 19:52:49 +0300

    HW2. react homeworks: HW2 верстка

--- src/p2-homeworks/h2/HW2.tsx
+++ src/p2-homeworks/h2/HW2.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import s from './HW2.module.css'
 import Affairs from './Affairs'
 
 export type AffairPriorityType = 'high' | 'middle' | 'low' // need to fix any
@@ -10,19 +11,20 @@ export type AffairType = {
 export type FilterType = 'all' | AffairPriorityType
 
 const defaultAffairs: Array<AffairType> = [ // need to fix any
-       {_id: 1, name: 'React', priority: 'high'},
-        {_id: 2, name: 'anime', priority: 'low'},
-        {_id: 3, name: 'games', priority: 'low'},
-        {_id: 4, name: 'work', priority: 'high'},
-        {_id: 5, name: 'html & css', priority: 'middle'},
+    {_id: 1, name: 'React', priority: 'high'},
+    {_id: 2, name: 'anime', priority: 'low'},
+    {_id: 3, name: 'games', priority: 'low'},
+    {_id: 4, name: 'work', priority: 'high'},
+    {_id: 5, name: 'html & css', priority: 'middle'},
 ]
 export const filterAffairs = (affairs: Array<AffairType>, filter: FilterType): Array<AffairType> => { // need to fix any
     if (filter === 'all') return affairs
-       else return affairs.filter(f => f.priority === filter)
+    else return affairs.filter(f => f.priority === filter)
 }
 export const deleteAffair = (affairs: AffairType[], _id: number): AffairType[] => {
     return affairs.filter(d => d._id != _id)
 }
+
 function HW2() {
     const [affairs, setAffairs] = useState<AffairType[]>(defaultAffairs) // need to fix any
 
@@ -32,9 +34,9 @@ function HW2() {
     const deleteAffairCallback = (_id: number) => setAffairs(deleteAffair(affairs, _id)) // need to fix any
 
     return (
-        <div>
-            <hr/>
-            homeworks 2
+        <div className={s.hw}>
+
+          <h2>homeworks 2</h2>
 
             {/*should work (должно работать)*/}
             <Affairs
@@ -44,10 +46,8 @@ function HW2() {
                 deleteAffairCallback={deleteAffairCallback}
             />
 
-            <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeAffairs/>*/}
-            <hr/>
         </div>
     )
 }
